60ba1f569c17ccf676889749f411cd47439ebaba
nox1d <114980767+nox1d@users.noreply.github.com>
2025-04-01 13:54:24 +0800

    Make player move forward based on direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,19 +3,15 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 
+func _ready():
+	await get_tree().create_timer(3).timeout
+	velocity = Vector2(1, 0).normalized() * SPEED
 
 func _physics_process(delta):
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
+	direction = direction.normalized()
+	
 	if direction:
 		velocity = direction * SPEED
 		print(velocity)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
