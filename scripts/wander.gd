e46c88ecee44283b0faa839116fa4cb09c2c6be5
nox1d <114980767+nox1d@users.noreply.github.com>
2025-04-04 16:39:37 +0800

    Replace enemy node with CharacterBody2D, add wander state

---
+++ scripts/wander.gd
@@ -0,0 +1,36 @@
+extends State
+
+@export var enemy: CharacterBody2D
+@export var move_speed : float = 80.0
+@onready var animation_tree = $"../../AnimationTree"
+
+var direction : Vector2
+var wander_time : float
+
+func randomize_wander():
+	direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+	if direction.y > direction.x:
+		direction.x = 0
+	else:
+		direction.y = 0
+	direction = direction.normalized()
+	print(direction)
+	wander_time = randf_range(1, 2)
+
+func Enter():
+	randomize_wander()
+
+func Update(delta):
+	if wander_time > 0:
+		wander_time -= delta
+	else:
+		randomize_wander()
+
+func Physics_Update(delta):
+	if direction == Vector2.ZERO:
+		pass
+	else:
+		animation_tree.set("parameters/walk/blend_position", direction)
+	if enemy:
+		enemy.velocity = direction * move_speed
+	
